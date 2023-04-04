9ddac82ba90c44dfae06b21c2c7d497c2961072e
SrishtiChatterjee <102905446+SrishtiChatterjee@users.noreply.github.com>
2023-04-04 14:24:22 +0530

    Update chefland games.c

--- codechef/chefland games.c
+++ codechef/chefland games.c
@@ -3,14 +3,13 @@
 int main(void) {
 	int n;
 	scanf("%d",&n);
-	for(int i=0,i<n;i++){
+	for(int i=0;i<n;i++){
 	    int a,b,c,d;
 	    scanf("%d %d %d %d",&a,&b,&c,&d);
-	    if(a==b && b==c && c==d)
-	    printf("IN");
+	    if(a==0 && a==b && b==c && c==d) //or if(a+b+c+d == 0)
+	    printf("IN \n");
 	    else
-	    printf("OUT");
+	    printf("OUT \n");
 	}
 	return 0;
 }
-
